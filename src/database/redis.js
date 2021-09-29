64275115b6f4dfd05b6adfbad826e8a264f00af3
Samuel Osinloye <psalmueloye@gmail.com>
2021-09-29 18:11:48 +0100

    configure redis

--- src/database/redis.js
+++ src/database/redis.js
@@ -2,20 +2,18 @@ const redis = require("redis");
 
 //const client = redis.createClient(process.env.REDIS_URL);
 
-const redisClient;
-if (process.env.NODE_ENV === production) {
+let redisClient;
+
+if (process.env.NODE_ENV === "production") {
   redisClient = redis.createClient(process.env.REDIS_URL, {
     tls: {
-        rejectUnauthorized: false
+      rejectUnauthorized: false
     }
-});
+  });
 } else {
   redisClient = redis.createClient({
     port: 6379,
     host: "localhost"
   });
 }
-
-
-
 module.exports = redisClient;
